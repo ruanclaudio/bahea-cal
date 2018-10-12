4fe1e1edbdb7951e4b4bb528829fc2a0d04c53bb
Ivan Neto <ivan.cr.neto@gmail.com>
2018-10-12 19:08:49 -0300

    adjusted flow to use env vars

--- fetch.py
+++ fetch.py
@@ -1,22 +1,30 @@
 import arrow
+import os
 import requests
+import tempfile
 from bs4 import BeautifulSoup
 
-
+from io import StringIO
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
 
-# Setup the Calendar API
 SCOPES = 'https://www.googleapis.com/auth/calendar'
-store = file.Storage('token.json')
+
+token_name = os.environ.get('TOKEN_NAME')
+store = file.Storage(token_name)
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    with open(token_name, 'w+') as token_file:
+        token_file.write(os.environ.get('TOKEN_JSON'))
+        with tempfile.NamedTemporaryFile('w+', delete=False) as creds_file:
+            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
     creds = tools.run_flow(flow, store)
+    store.put(creds)
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
+now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 
 url = 'http://www.esporteclubebahia.com.br/agenda/'
 resp = requests.get(url)
@@ -51,8 +59,24 @@ for a in _as:
                 'timeZone': 'America/Bahia',
             },
         }
-        eventc = service.events().insert(
-            calendarId='primary', body=event).execute()
-        print('Event created: {} x {} {} {}'.format(
-            team1, team2, when,
-            eventc.get('htmlLink')))
+        possible_existing = service.events().list(
+            calendarId='primary', timeMin=when, maxResults=20,
+            singleEvents=True, orderBy='startTime',
+            timeMax=when.shift(minutes=+140)).execute()
+
+        existent = False
+        for possible in possible_existing.get('items', []):
+            if possible['summary'] == event['summary']:
+                existent = True
+                break
+
+        if not existent:
+            eventc = service.events().insert(
+                calendarId='primary', body=event).execute()
+            print('Event created: {} x {} {} {}'.format(
+                team1, team2, when,
+                eventc.get('htmlLink')))
+        else:
+            print('Event already in calendar: {} x {} {} {}'.format(
+                team1, team2, when,
+                possible.get('htmlLink')))
