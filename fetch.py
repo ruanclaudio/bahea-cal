2bb356376c8a4579756cce09e12e67004c35e66a
Ivan Neto <ivan.cr.neto@gmail.com>
2024-02-28 17:19:19 -0300

    new way of fetching and parsing matches

--- fetch.py
+++ fetch.py
@@ -1,83 +1,134 @@
-import arrow
+import datetime
 import os
-import requests
 import tempfile
-from bs4 import BeautifulSoup
 
-from io import StringIO
+import arrow
+import attrs
 from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import datetime
 
-SCOPES = 'https://www.googleapis.com/auth/calendar'
 
-token_name = os.environ.get('TOKEN_NAME')
-store = file.Storage(token_name)
-creds = store.get()
-if not creds or creds.invalid:
-    with open(token_name, 'w+') as token_file:
-        token_file.write(os.environ.get('TOKEN_JSON'))
-        with tempfile.NamedTemporaryFile('w+', delete=False) as creds_file:
-            flow = client.flow_from_clientsecrets(
-                os.environ.get('CREDENTIALS_JSON'), SCOPES)
-    creds = tools.run_flow(flow, store)
-    store.put(creds)
-service = build('calendar', 'v3', http=creds.authorize(Http()))
+def get_service():
+    SCOPES = "https://www.googleapis.com/auth/calendar"
 
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    # import ipdb; ipdb.set_trace()
 
-url = 'http://www.esporteclubebahia.com.br/agenda/'
-resp = requests.get(url)
-soup = BeautifulSoup(resp.content, 'html.parser')
-lis = soup.findAll('li')
-_as = []
-for li in lis:
-    _as.append(li.find('a'))
-for a in _as:
-    if '/agenda/' in a.get('href', ''):
-        if not a.find('div', {'class': 'times'}):
-            continue
-        team1 = a.find('div', {'class': 'times'}).findAll('span')[0].text
-        team2 = a.find('div', {'class': 'times'}).findAll('span')[1].text
-        spplited = a.find(
-            'div', {'class': 'data'}).find('span').text.split('•')
-        date = spplited[0].strip()
-        time = spplited[1].strip()
-        when = arrow.get(
-            date + ' ' + time + ' America/Bahia', 'DD/MM HH:mm ZZZ')
-        when = when.replace(year=arrow.utcnow().year)
-        event = {
-            'summary': '{} x {}'.format(team1, team2),
-            'location': '',
-            'description': '{} x {}'.format(team1, team2),
-            'start': {
-                'dateTime': when.isoformat(),
-                'timeZone': 'America/Bahia',
+    token_name = os.environ.get("TOKEN_NAME")
+    store = file.Storage(token_name)
+    creds = store.get()
+    if not creds or creds.invalid:
+        with open(token_name, "w+") as token_file:
+            token_file.write(os.environ.get("TOKEN_JSON"))
+            with tempfile.NamedTemporaryFile("w+", delete=False) as creds_file:
+                flow = client.flow_from_clientsecrets(os.environ.get("CREDENTIALS_JSON"), SCOPES)
+        creds = tools.run_flow(flow, store)
+        store.put(creds)
+    service = build("calendar", "v3", http=creds.authorize(Http()))
+    return service
+
+
+now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+
+
+@attrs.define
+class CalendarEvent:
+    summary: str
+    description: str
+    start_datetime: arrow.Arrow
+    start_timezone: str
+    end_datetime: arrow.Arrow
+    end_timezone: str
+    location: str = attrs.field(default=None)
+
+    @classmethod
+    def from_parsed(cls, event):
+        match = event.match
+
+        timezone = "America/Bahia"
+        start_date = arrow.get(f"{match.startDate} {match.startHour} {timezone}", "YYYY-MM-DD HH:mm:ss ZZZ")
+        return cls(
+            summary=f"{match.homeTeam} x {match.awayTeam}",
+            description=f"{match.championship}: {match.homeTeam} x {match.awayTeam}, {match.location}",
+            start_datetime=start_date,
+            start_timezone=timezone,
+            end_datetime=start_date.replace(minutes=+110),
+            end_timezone=timezone,
+            location=match.location,
+        )
+        # self.__typename = kwargs.pop("__typename")
+        # self.awayTeam = kwargs.pop("awayTeam")
+        # self.championship = kwargs.pop("championship")
+        # self.homeTeam = kwargs.pop("homeTeam")
+        # self.liveWatchSources = kwargs.pop("liveWatchSources")
+        # self.location = kwargs.pop("location")
+        # self.phase = kwargs.pop("phase")
+        # self.round = kwargs.pop("round")
+        # self.scoreboard = kwargs.pop("scoreboard")
+        # self.startDate = kwargs.pop("startDate")
+        # self.startHour = kwargs.pop("startHour")
+        # self.transmission = kwargs.pop("transmission")
+        # self.winner = kwargs.pop("winner")
+
+    def as_dict(self):
+        return {
+            "summary": self.summary,
+            "location": str(self.location),
+            "description": self.description,
+            "start": {
+                "dateTime": self.start_datetime.isoformat(),
+                "timeZone": self.start_timezone,
             },
-            'end': {
-                'dateTime': when.replace(minutes=+110).isoformat(),
-                'timeZone': 'America/Bahia',
+            "end": {
+                "dateTime": self.end_datetime.isoformat(),
+                "timeZone": self.end_timezone,
             },
         }
-        possible_existing = service.events().list(
-            calendarId='primary', timeMin=when, maxResults=20,
-            singleEvents=True, orderBy='startTime',
-            timeMax=when.shift(minutes=+140)).execute()
+
+
+def schedule():
+    from parse import parse
+
+    service = get_service()
+
+    soccer_events = parse()
+    events = soccer_events
+
+    for event in events:
+        try:
+            event = CalendarEvent.from_parsed(event)
+        except:
+            print(f"Skipping: {event.match.homeTeam} x {event.match.awayTeam}")
+            continue
+
+        when = event.start_datetime
+
+        possible_existing = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=when,
+                maxResults=20,
+                singleEvents=True,
+                orderBy="startTime",
+                timeMax=when.shift(minutes=+140),
+            )
+            .execute()
+        )
 
         existent = False
-        for possible in possible_existing.get('items', []):
-            if possible['summary'] == event['summary']:
+        for possible in possible_existing.get("items", []):
+            if possible["summary"] == event.summary:
                 existent = True
                 break
 
         if not existent:
-            eventc = service.events().insert(
-                calendarId='primary', body=event).execute()
-            print('Event created: {} x {} {} {}'.format(
-                team1, team2, when,
-                eventc.get('htmlLink')))
+            event_dict = event.as_dict()
+            eventc = service.events().insert(calendarId="primary", body=event_dict).execute()
+            print(f"Event created: {event.description} {eventc.get('htmlLink')}")
         else:
-            print('Event already in calendar: {} x {} {} {}'.format(
-                team1, team2, when,
-                possible.get('htmlLink')))
+            print(f"Event already in calendar: {event.description} {possible.get('htmlLink')}")
+
+
+if __name__ == "__main__":
+    schedule()
---
+++ parse.py
@@ -0,0 +1,179 @@
+import collections
+import json
+import re
+
+import requests
+
+collections.Callable = collections.abc.Callable
+
+from bs4 import BeautifulSoup
+
+
+class Team:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.badgePng = kwargs.pop("badgePng")
+        self.badgeSvg = kwargs.pop("badgeSvg")
+        self.id = kwargs.pop("id")
+        self.popularName = kwargs.pop("popularName")
+
+    def __str__(self):
+        return f"{self.popularName}"
+
+
+class Championship:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.name = kwargs.pop("name")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class MatchLocation:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.name = kwargs.pop("name")
+        self.popularName = kwargs.pop("popularName")
+
+    def __str__(self):
+        return f"{self.popularName}"
+
+
+class Phase:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.name = kwargs.pop("name")
+        self.type = kwargs.pop("type")
+
+
+class Scoreboard:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.away = kwargs.pop("away")
+        self.home = kwargs.pop("home")
+        self.penalty = kwargs.pop("penalty")
+
+
+class TRTransmission:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.broadcastStatus = kwargs.pop("broadcastStatus")
+        self.featuredVideo = kwargs.pop("featuredVideo")
+        self.id = kwargs.pop("id")
+        self.period = kwargs.pop("period")
+        self.url = kwargs.pop("url")
+
+
+class BroadcastStatus:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.id = kwargs.pop("id")
+        self.label = kwargs.pop("label")
+
+
+class Period:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.id = kwargs.pop("id")
+        self.label = kwargs.pop("label")
+
+
+class Match:
+    def __init__(self, **kwargs):
+        self.__typename = kwargs.pop("__typename")
+        self.awayTeam = kwargs.pop("awayTeam")
+        self.championship = kwargs.pop("championship")
+        self.homeTeam = kwargs.pop("homeTeam")
+        self.liveWatchSources = kwargs.pop("liveWatchSources")
+        self.location = kwargs.pop("location")
+        self.phase = kwargs.pop("phase")
+        self.round = kwargs.pop("round")
+        self.scoreboard = kwargs.pop("scoreboard")
+        self.startDate = kwargs.pop("startDate")
+        self.startHour = kwargs.pop("startHour")
+        self.transmission = kwargs.pop("transmission")
+        self.winner = kwargs.pop("winner")
+
+
+class SoccerEvent:
+    def __init__(self, **kwargs):
+        self.match = kwargs.pop("match")
+
+
+def instantiate_from_json(data):
+    if isinstance(data, dict):
+        typename = data.get("__typename")
+        if typename == "Team":
+            return Team(**data)
+        elif typename == "Championship":
+            return Championship(**data)
+        elif typename == "MatchLocation":
+            return MatchLocation(**data)
+        elif typename == "Phase":
+            return Phase(**data)
+        elif typename == "Scoreboard":
+            return Scoreboard(**data)
+        elif typename == "TRTransmission":
+            broadcastStatus = instantiate_from_json(data.pop("broadcastStatus"))
+            period = instantiate_from_json(data.pop("period"))
+            return TRTransmission(broadcastStatus=broadcastStatus, period=period, **data)
+        elif typename == "BroadcastStatus":
+            return BroadcastStatus(**data)
+        elif typename == "Period":
+            return Period(**data)
+        elif typename == "Match":
+            awayTeam = instantiate_from_json(data.pop("awayTeam"))
+            championship = instantiate_from_json(data.pop("championship"))
+            homeTeam = instantiate_from_json(data.pop("homeTeam"))
+            location = instantiate_from_json(data.pop("location"))
+            phase = instantiate_from_json(data.pop("phase"))
+            scoreboard = instantiate_from_json(data.pop("scoreboard"))
+            transmission = instantiate_from_json(data["transmission"]) if data["transmission"] else None
+            data.pop("transmission")
+            return Match(
+                awayTeam=awayTeam,
+                championship=championship,
+                homeTeam=homeTeam,
+                location=location,
+                phase=phase,
+                scoreboard=scoreboard,
+                transmission=transmission,
+                **data,
+            )
+        elif typename == "SoccerEvent":
+            match = instantiate_from_json(data.pop("match"))
+            return SoccerEvent(match=match)
+    elif isinstance(data, list):
+        return [instantiate_from_json(item) for item in data]
+    return data
+
+
+def parse():
+    url = "https://ge.globo.com/ba/futebol/times/bahia/agenda-de-jogos-do-bahia/"
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+    pattern = re.compile(r"window\.dataSportsSchedule = (\{.*?\});", re.DOTALL)
+
+    js_object_str = ""
+    for script in soup.find_all("script"):
+        if script.string and pattern.search(script.string):
+            js_object_str = pattern.search(script.string).group(1)
+            break
+
+    if js_object_str:
+        js_object_str = js_object_str.replace("CHAMPIONSHIPS", '"CHAMPIONSHIPS"')
+        js_object_str = js_object_str.replace("MULTISPORT", '"MULTISPORT"')
+        js_object_str = js_object_str.replace("SCHEDULE_TEAM", '"SCHEDULE_TEAM"')
+        js_object_str = js_object_str.replace("INFO_TEAM", '"INFO_TEAM"')
+
+        try:
+            parsed_data = json.loads(js_object_str)
+        except json.JSONDecodeError as e:
+            print("Error decoding JSON:", e)
+        else:
+            soccer_events = instantiate_from_json(parsed_data["SCHEDULE_TEAM"]["teamAgenda"]["future"])
+            return soccer_events
+    else:
+        print("JavaScript object not found in the HTML content")
