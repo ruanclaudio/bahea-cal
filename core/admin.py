85a27c00360f5f01c760b672491eec229eb4d179
Ivan Neto <ivan.cr.neto@gmail.com>
2024-02-28 20:56:02 -0300

    added models, admin and migrations

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Team, Championship, Location, Phase, Round, Match, SoccerEvent
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("popular_name",)
+    search_fields = ("popular_name",)
+
+
+@admin.register(Championship)
+class ChampionshipAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "popular_name")
+    search_fields = ("name", "popular_name")
+
+
+@admin.register(Phase)
+class PhaseAdmin(admin.ModelAdmin):
+    list_display = ("name", "phase_type")
+    search_fields = ("name", "phase_type")
+
+
+@admin.register(Round)
+class RoundAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ("championship", "home_team", "away_team", "location", "phase", "round", "start_at")
+    search_fields = (
+        "championship__name",
+        "home_team__popular_name",
+        "away_team__popular_name",
+        "location__popular_name",
+        "phase__name",
+        "round__name",
+    )
+    list_filter = ("championship", "home_team", "away_team", "location", "phase", "round")
+
+
+@admin.register(SoccerEvent)
+class SoccerEventAdmin(admin.ModelAdmin):
+    list_display = ("match",)
+    search_fields = ("match__id",)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,58 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    popular_name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f"<Team: {self.popular_name}>"
+
+
+class Championship(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return f"<Championship: {self.name}>"
+
+
+class Location(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True)
+    popular_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"<MatchLocation: {self.popular_name}>"
+
+
+class Phase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True)
+    phase_type = models.CharField(max_length=64)
+
+
+class Round(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Match(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    championship = models.ForeignKey("core.Championship", on_delete=models.SET_NULL, null=True)
+    home_team = models.ForeignKey("core.Team", on_delete=models.CASCADE, related_name="home_matches")
+    away_team = models.ForeignKey("core.Team", on_delete=models.CASCADE, related_name="away_matches")
+    location = models.ForeignKey("core.Location", on_delete=models.SET_NULL, null=True)
+    phase = models.ForeignKey("core.Phase", on_delete=models.SET_NULL, null=True)
+    round = models.ForeignKey("core.Round", on_delete=models.SET_NULL, null=True)
+    start_at = models.DateTimeField()
+
+
+class SoccerEvent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    match = models.ForeignKey("core.Match", on_delete=models.CASCADE, related_name="event")
