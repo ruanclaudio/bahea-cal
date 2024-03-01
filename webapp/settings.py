5fd90ae6d475ab77e94765d740a998eb7d389495
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-01 09:55:16 -0300

    typo

--- webapp/settings.py
+++ webapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = djsettings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = djsettings.get("DEBUG", False, cast=bool)
+DEBUG = djsettings.get("DEBUG", False, cast='@bool')
 
 ALLOWED_HOSTS = djsettings.ALLOWED_HOSTS
 
