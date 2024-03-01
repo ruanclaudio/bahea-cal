61c7a4527ef46e20d7439deedabb0cc0a98866a5
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-01 08:54:33 -0300

    get settings from dynaconf

---
+++ config.py
@@ -0,0 +1,8 @@
+
+from dynaconf import Dynaconf
+
+djsettings = Dynaconf(
+    envvar_prefix="DJANGO",
+    settings_files=['settings.env', '.secrets.env'],
+    load_dotenv=True,
+)
--- webapp/settings.py
+++ webapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from config import djsettings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-btjo6x^cvh1#srimom#couy&qs92ozh6t@#n4n9@%9%-ps_71g"
+SECRET_KEY = djsettings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = djsettings.ALLOWED_HOSTS
 
 
 # Application definition
@@ -76,8 +77,8 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": djsettings.DATABASES__default__ENGINE,
+        "NAME": djsettings.DATABASES__default__NAME,
     }
 }
 
