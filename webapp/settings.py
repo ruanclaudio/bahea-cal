f908b4f31053d4eef8e2ef2ed9bb5115905d38be
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-02 16:22:10 -0300

    keeping sqlite db for now

--- webapp/settings.py
+++ webapp/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": djsettings.DATABASES__default__ENGINE,
+#         "NAME": djsettings.DATABASES__default__NAME,
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": djsettings.DATABASES__default__ENGINE,
-        "NAME": djsettings.DATABASES__default__NAME,
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / 'db.sqlite3',
     }
 }
 
