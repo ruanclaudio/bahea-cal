eb3e03070142771c20c21666b5841cb3e218b822
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-06 20:46:26 -0300

    add db settings

--- webapp/settings.py
+++ webapp/settings.py
@@ -75,19 +75,26 @@ WSGI_APPLICATION = "webapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": djsettings.DATABASES__default__ENGINE,
-#         "NAME": djsettings.DATABASES__default__NAME,
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / 'db.sqlite3',
+        "ENGINE": djsettings.DATABASES__default__ENGINE,
+        "NAME": djsettings.DATABASES__default__NAME,
+        'USER': djsettings.DATABASES__default__USER,
+        'PASSWORD': djsettings.DATABASES__default__PASSWORD,
+        'HOST': djsettings.DATABASES__default__HOST,
+        'PORT': djsettings.DATABASES__default__PORT,
+        'OPTIONS': djsettings.DATABASES__default__OPTIONS,
     }
 }
+
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": djsettings.DATABASES__default__ENGINE,
+            "NAME": djsettings.DATABASES__default__NAME,
+        }
+    }
+
 
 
 # Password validation
