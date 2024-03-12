81419a711a0b49ba0051747d60f55cceccc15d4a
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-12 15:44:13 -0300

    function to get secrets from aws

---
+++ webapp/secrets.py
@@ -0,0 +1,27 @@
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+
+def get_secret(secret_name, region_name="sa-east-1"):
+
+    secret_name = "staging/google/calendar"
+    region_name = "sa-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    return json.loads(json.loads(get_secret_value_response['SecretString'])['credentials'])
