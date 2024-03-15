708f0237e94ef8bd73f88be720a12e6ee072a1f2
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-15 14:16:40 -0300

    add admin entries for new models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import UserCredential
+from .models import UserEvent, UserSubscription, User
 
 
 @admin.register(UserCredential)
@@ -7,3 +10,25 @@ class UserCredentialAdmin(admin.ModelAdmin):
     list_display = ("client_id", "user")
     search_fields = ("user", "client_id")
     readonly_fields = ("credentials",)
+
+
+@admin.register(UserEvent)
+class UserEventAdmin(admin.ModelAdmin):
+    list_display = ("eid", "user", "event")
+    search_fields = ("eid", "user__username", "event__name")
+
+
+@admin.register(UserSubscription)
+class UserSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("team", "user")
+    search_fields = ("team__popular_name", "user__username")
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # fieldsets = BaseUserAdmin.fieldsets + (("Additional Info", {"fields": ("events", "subscriptions")}),)
+    # filter_horizontal = (
+    #     "events",
+    #     "subscriptions",
+    # )
+    ...
