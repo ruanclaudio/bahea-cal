d1bc11ad7f82d261c24299bce74f5278f7a29c4a
Ivan Neto <ivan.cr.neto@gmail.com>
2024-03-30 20:48:41 -0300

    added user view to confirm subscription

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
+from django.core.exceptions import BadRequest
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+from core.models import Team
+
+
+@require_http_methods(["POST"])
+def confirm(request):
+    if not request.user.is_authenticated:
+        raise BadRequest("Usuário não autenticado")
+
+    if not (subscription := request.POST.get("subscription")):
+        raise BadRequest("Inscrição não informada")
+
+    try:
+        team = Team.objects.get(ref=subscription)
+    except Team.DoesNotExist:
+        raise BadRequest("Inscrição não encontrada")
+    else:
+        request.user.subscriptions.add(team)
+
+    return render(request, "users/confirmed.html")
