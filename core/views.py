27d0870e3ca316973504ac3773e27fc8eda77104
Ruan <ruanclaudio4@gmail.com>
2024-04-26 15:42:13 -0300

    fix:modifies login flow

--- core/views.py
+++ core/views.py
@@ -30,6 +30,7 @@ def home(request):
 
 
 def google_calendar_init_view(request):
+    print(settings.ENVIRONMENT)
     config = get_secret(f"{settings.ENVIRONMENT}/google/calendar")
     creds = CredentialsService.init_for(request.user, scopes=SCOPES)
     if not creds or not creds.valid:
@@ -40,7 +41,8 @@ def google_calendar_init_view(request):
             flow.redirect_uri = REDIRECT_URL
 
             authorization_url, state = flow.authorization_url(
-                access_type="offline", prompt="consent", include_granted_scopes="true"
+                access_type="offline",
+                prompt="consent", include_granted_scopes="true"
             )
             request.session["state"] = state
 
@@ -50,7 +52,7 @@ def google_calendar_init_view(request):
 
 
 def google_calendar_redirect_view(request):
-    state = request.session["state"]
+    state = request.GET.get("state")
     if state is None:
         return render(request, "core/success.html", {"error": "Algo de errado aconteceu."})
 
--- webapp/secrets.py
+++ webapp/secrets.py
@@ -1,22 +1,31 @@
 import boto3
+import os
 from botocore.exceptions import ClientError
 import json
 
 
 def get_secret(secret_name, region_name="sa-east-1"):
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
+    if "dev" in secret_name:
+        try:
+            with open(os.path.join(os.getcwd(), 'webapp/secrets.json'), 'r') as f:
+                secrets = json.load(f)
+                return secrets[secret_name]
+        except Exception as e_:
+            raise e_ from e_
+    else:
+        session = boto3.session.Session()
+        client = session.client(
+            service_name='secretsmanager',
+            region_name=region_name
         )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
 
-    return json.loads(json.loads(get_secret_value_response['SecretString'])['credentials'])
+        try:
+            get_secret_value_response = client.get_secret_value(
+                SecretId=secret_name
+            )
+        except ClientError as e:
+            # For a list of exceptions thrown, see
+            # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            raise e
+        
+        return json.loads(json.loads(get_secret_value_response['SecretString'])['credentials'])
