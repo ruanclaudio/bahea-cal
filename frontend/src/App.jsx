e3c67e194d9eca39139355d8c6e36a4a0dd5d0c6
kauan nery <113049473+Kauannery@users.noreply.github.com>
2024-04-26 15:05:52 -0300

    fuxo de login no front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,87 +1,64 @@
-//import React from 'react';
-import bahia from './assets/bahia.png';
-import flamengo1 from './assets/flamengo1.png';
-import palmeiras from './assets/palmeiras.png';
 import ApiCalendar from "react-google-calendar-api";
 import React from 'react';
-
 const config = {
-  clientId: process.env.GOOGLE_CLIENT_ID, 
-  apiKey: process.env.GOOGLE_API_KEY, 
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  apiKey: process.env.GOOGLE_API_KEY,
   scope: "https://www.googleapis.com/auth/calendar",
   discoveryDocs: [
     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
   ],
 };
-
-const apiCalendar = new ApiCalendar(config); 
-
- //* class DoubleButton extends React.Component {
+const apiCalendar = new ApiCalendar(config);
+export default class DoubleButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleItemClick = this.handleItemClick.bind(this);
   }
-  
-    handleItemClick(event, name) {
+  handleItemClick(event, name) {
     if (name === 'sign-in') {
-      apiCalendar.handleAuthClick()
+    console.log( apiCalendar.handleAuthClick())
+
     } else if (name === 'sign-out') {
-      apiCalendar.handleSignoutClick();
-    }
+      apiCalendar.handleSignoutClick();     }
   }
-} 
-
-
-
-
-const App = () => {
-  // Função para mostrar o escudo quando o botão é clicado
-  const showLogo = (teamName) => {
-    alert(`Você selecionou o escudo do ${teamName}`);
-    // Aqui você pode adicionar lógica para exibir o escudo
+  render() {
+    return (
+      <div>
+          <button
+              onClick={(e) => this.handleItemClick(e, 'sign-in')}
+              
+          >
+            sign-in
+          </button>
+          <button
+              onClick={(e) => this.handleItemClick(e, 'sign-out')}
+          >
+            sign-out
+          </button>
+       </div>
+      );
   }
-
-  return (
-    
-
-    <div>
-
-<button
-                  onClick={(e) => this.handleItemClick(e, 'sign-in')}
-              >
-                sign-in
-              </button>
-              <button
-                  onClick={(e) => this.handleItemClick(e, 'sign-out')}
-              >
-                sign-out
-              </button>
-
-
-
-
-      <h1>Escolha seu time favorito para Acompanhar:
-             </h1>
-     
-     <div className="logo-button"> 
-      
-      <button onClick={() => showLogo('bahia')}>
-        <img src={bahia} ></img>
-             Bahia
-      </button>
-      <button onClick={() => showLogo('Barcelona')}>
-      <img src={flamengo1} ></img>
-        Flamengo 
-      </button>
-      <button onClick={() => showLogo('Liverpool')}>
-      <img src={palmeiras} ></img>
-        Palmeiras 
-      </button>
-
-    </div>
-      {/* Adicione mais botões conforme necessário */}
-    </div>
-  );
 }
-
-export default App;
+// import logo from './logo.svg';
+// import './App.css';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
--- frontend/src/components/googleCalendar.js
+++ frontend/src/components/googleCalendar.js
@@ -11,5 +11,35 @@ const config = {
 
 const apiCalendar = new ApiCalendar(config); 
 
-
+const  eventFromNow : object  =  { 
+    sumário : "Poc Dev From Now" , 
+    hora : 480 , 
+  } ;
+  
+  apiCalendar 
+    . createEventFromNow ( eventoFromNow ) 
+    . então ( ( resultado : objeto )  = >  { 
+      console.log ( resultado ) ; } ) .​ catch ( ( erro : qualquer ) = > { console.log ( erro ) ; } ) ;​
+    
+      
+      
+    
+  
+  const  eventWithVideoConference : object  =  { 
+    sumário : "Evento com conferência do Google Meet" , 
+    início : { 
+      dateTime : nova  data ( ) . toISOString ( ) , 
+      timeZone : "Europa/Paris" , 
+    } , 
+    end : { 
+      dateTime : new  Date ( new  Date ( ) . getTime ( )  +  3600000 ) . toISOString ( ) , 
+      timeZone : "Europa/Paris" , 
+    } , 
+  } ;
+  
+  apiCalendar 
+    . createEventWithVideoConference ( eventWithVideoConference ) 
+    . então ( ( resultado : objeto )  = >  { 
+      console.log ( resultado ) ; } ) .​ catch ( ( erro : qualquer ) = > { console.log ( erro ) ; } ) ;​
+    
 
