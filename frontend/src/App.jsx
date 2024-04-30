dfc53ac418a73a6da30f1d34759e8bd90a1b8ac7
kauan nery <113049473+Kauannery@users.noreply.github.com>
2024-04-30 13:53:56 -0300

    tela de login adicionada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,15 @@
-import ApiCalendar from "react-google-calendar-api";
+
 import React from 'react';
-const config = {
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  apiKey: process.env.GOOGLE_API_KEY,
-  scope: "https://www.googleapis.com/auth/calendar",
-  discoveryDocs: [
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  ],
-};
-const apiCalendar = new ApiCalendar(config);
+
+
 export default class DoubleButton extends React.Component {
   constructor(props) {
     super(props);
     this.handleItemClick = this.handleItemClick.bind(this);
   }
   handleItemClick(event, name) {
-    if (name === 'sign-in') {
-    console.log( apiCalendar.handleAuthClick())
-
-    } else if (name === 'sign-out') {
-      apiCalendar.handleSignoutClick();     }
+    console.log(event, name) 
+   
   }
   render() {
     return (
@@ -39,26 +29,3 @@ export default class DoubleButton extends React.Component {
       );
   }
 }
-// import logo from './logo.svg';
-// import './App.css';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// export default App;
