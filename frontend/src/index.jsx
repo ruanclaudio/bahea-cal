4c8e65880a55897909965b58cdf93b32450bf90e
kauan nery <113049473+Kauannery@users.noreply.github.com>
2024-05-03 17:29:55 -0300

    Issue: Rotas criadas para diferentes interfaces

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Time from "./routes/Time.jsx";
 import './index.css'
+
+const router = createBrowserRouter([
+
+  {
+      path:"/",
+      element: <App/>,
+
+  },
+  {
+    path: "/hello",
+    element: <div> hello</div>,
+  },
+  {
+    path: "/Time",
+    element: <Time />,
+  },
+
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+  
   </React.StrictMode>,
 )
