bf14de700596fbf670776ce96c0e725549947fee
Ruan <ruanclaudio4@gmail.com>
2024-05-08 16:49:05 -0300

    feat: api google calendar path

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from api import views
+
+urlpatterns = [
+    path('v1/calendar/init/', views.calendar_init_view, name='calendar_init'),
+    path('v1/calendar/flow/', views.calendar_flow_view, name='calendar_flow'),
+    path('test-drf/', views.test_drf, name="test-drf"),
+]
