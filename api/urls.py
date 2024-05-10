f5da02f21a3c6afa58563f8ba2b5ceb0efec8234
Ruan <ruanclaudio4@gmail.com>
2024-05-10 15:14:04 -0300

    remove: test-drf path

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,4 @@ from api import views
 urlpatterns = [
     path('v1/calendar/init/', views.calendar_init_view, name='calendar_init'),
     path('v1/calendar/flow/', views.calendar_flow_view, name='calendar_flow'),
-    path('test-drf/', views.test_drf, name="test-drf"),
 ]
