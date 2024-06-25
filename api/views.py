0d7ac7302403de51af12699486d3cc00b5c777b2
Ruan <ruanclaudio4@gmail.com>
2024-06-25 15:26:46 -0300

    feat: add user profile endpoint

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ import os.path
 
 from django.conf import settings
 from django.contrib.auth import login
+from django.shortcuts import redirect
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from core.views import UserService
@@ -16,7 +17,10 @@ from google_auth_oauthlib.flow import Flow
 from google.auth.transport import Request
 from google.oauth2 import service_account
 from googleapiclient.discovery import build as apiClientBuild
+import googleapiclient.discovery
 from pathlib import Path
+
+
 
 ROOT_FOLDER = Path().resolve().parent
 FULL_SECRET_PATH = os.path.join(ROOT_FOLDER, "webapp", "secrets.json")
@@ -79,20 +83,79 @@ def calendar_token(request):
 @api_view(["GET"])
 def user_info_view(request):
     user = request.user
-    user_creds = UserCredential.objects.get(user=request.user)
+    print('user == ', user.email)
+    print('user fname== ', user.first_name)
+    print('user lname == ', user.last_name)
+    print('user id == ', user.id)
+    print('user id == ', user.acessToken)
+    # print('locale == ', user.photo)
+    # print('locale == ', user.locale)
+    # print('user pimage == ', user.picture)
+    # print('request == ', request_data)
+       
+    user_creds = UserCredential.objects.get(user)
     creds = Credentials.from_user_credentials(user_creds)
     
-    calendar_service = build('calendar', 'v3', credentials=creds)
+    calendar_service = googleapiclient.discovery.build('calendar', 'v3', credentials=creds)
     selected_teams = {}
-    time_to_match = {}
+    notify_before = {}
         
     user_info = {
         'first_name': user.first_name,
         'last_name': user.last_name,
         'email': user.email,
-        'photo': user.profile.photo,
+        'photo': user.photo,
         'selected_teams': selected_teams,
-        'time_to_match': time_to_match
+        'notify_before': notify_before
     }
 
     return JsonResponse(user_info)
+
+import requests
+from google.auth.transport import requests as google_requests
+@api_view(["GET"])
+def test_return(request):
+
+    CLIENT_SECRETS_FILE = os.path.join(ROOT_FOLDER, "bahea-cal/webapp", "secrets2.json")
+
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    print(flow)
+    creds = flow.run_local_server(port=0)
+    def verify_id_token(id_token):
+        try:
+            # Specify the CLIENT_ID of the app that accesses the backend:
+            CLIENT_ID = "600789865643-nh6ss44i17aov60br8fno3hqoihro4uh.apps.googleusercontent.com"
+            # Verify the token
+            idinfo = id_token.verify_oauth2_token(id_token, google_requests.Request(), CLIENT_ID)
+            # ID token is valid
+            return idinfo
+        except ValueError:
+            # Invalid token
+            return None
+
+
+      # Get the ID token from the credentials
+    id_token_info = creds.id_token
+    # Verify the ID token
+    idinfo = verify_id_token(id_token_info)
+    
+    if idinfo:
+        userinfo = {
+            'sub': idinfo['sub'],
+            'name': idinfo['name'],
+            'given_name': idinfo['given_name'],
+            'family_name': idinfo['family_name'],
+            'picture': idinfo['picture'],
+            'email': idinfo['email'],
+            'email_verified': idinfo['email_verified'],
+            'locale': idinfo['locale']
+        }
+    
+        print("User info:", json.dumps(userinfo, indent=4)) 
+        return JsonResponse({ "userInfo":json.dumps(userinfo, indent=4) })
+
+    # url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=access_token"
+    # response = requests.get(url)
+    
+    # data = json.loads(response.text)
+    # return JsonResponse(data)
