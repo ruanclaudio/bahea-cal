6cc53dea9d51fe2cf770229d6869fc2d27044de5
Ruan <ruanclaudio4@gmail.com>
2024-07-01 17:06:58 -0300

    remove: comments into serializer.py

--- users/serializer.py
+++ users/serializer.py
@@ -13,13 +13,3 @@ class UserInfoSerializer(serializers.Serializer):
     class Meta:
         user_model = User
         fields = ["id", "email", "verified_email", "name", "given_name", "family_name", "picture"]
-    
-# {
-#     "id": "109776696914646781435",
-#     "email": "ruanclaudio4@gmail.com",
-#     "verified_email": true,
-#     "name": "Ruan Cláudio",
-#     "given_name": "Ruan",
-#     "family_name": "Cláudio",
-#     "picture": "https://lh3.googleusercontent.com/a/ACg8ocKoJXxaqmOY3mvQBIKRAxigf4fqC-H0g-6X0t051UYVPfZeaqQr=s96-c"
-# }
