9b01f8d21820eaac4c8d86545072b8ba7fcf12e7
Ruan Cláudio <ruanclaudio4@gmail.com>
2024-07-11 13:29:15 -0300

    fix: add sessionauthentication for run BE correctly

--- webapp/settings.py
+++ webapp/settings.py
@@ -70,6 +70,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
